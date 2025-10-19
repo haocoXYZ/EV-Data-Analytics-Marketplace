1d8f5d1fb7c5df36f3685146519ad45395f07dfc
Cơ <157873492+haocoXYZ@users.noreply.github.com>
2025-10-20 00:20:32 +0700

    Delete backend/SWP391_FA25-binding directory

--- backend/SWP391_FA25-binding/src/main/java/com/dangkhoa/superapp/swp391_fa25/service/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package com.dangkhoa.superapp.swp391_fa25.service;
-
-import com.dangkhoa.superapp.swp391_fa25.entity.Payment;
-import java.math.BigDecimal;
-
-public interface PaymentService {
-    Payment createPayment(int consumerId, BigDecimal amount, String method, String type, String note);}
