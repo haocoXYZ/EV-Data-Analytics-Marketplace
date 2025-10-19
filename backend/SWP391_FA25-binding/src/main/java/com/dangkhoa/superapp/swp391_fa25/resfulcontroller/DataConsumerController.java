2d1e9abc61d63723eac703d26658f4cbe8844093
Duong Hao Co <davidduong1711@gmail.com>
2025-10-19 16:13:27 +0700

    BE

---
+++ backend/SWP391_FA25-binding/src/main/java/com/dangkhoa/superapp/swp391_fa25/resfulcontroller/DataConsumerController.java
@@ -0,0 +1,36 @@
+package com.dangkhoa.superapp.swp391_fa25.resfulcontroller;
+
+
+import com.dangkhoa.superapp.swp391_fa25.entity.Dataset;
+import com.dangkhoa.superapp.swp391_fa25.entity.User;
+import com.dangkhoa.superapp.swp391_fa25.service.impl.DatasetServiceImpl;
+import com.dangkhoa.superapp.swp391_fa25.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/consumer")
+
+public class DataConsumerController {
+    // hóa đơn
+    // dataset
+    // map
+    // profile
+    @Autowired
+    private  UserServiceImpl service;
+    @Autowired
+    private  DatasetServiceImpl dataService;
+
+
+    @GetMapping("/consumer/dataset")
+    public List<Dataset> getAllDatasets() {
+        return dataService.getAllDatasets();
+    }
+
+
+}
